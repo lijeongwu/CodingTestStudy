f82c4d6119e55a26ab0e45d42e4574947f3df279
lijeongwu <dlwjddn5534@naver.com>
2024-09-24 20:59:48 +0900

    두수의곱구하기

---
+++ week1/problem3.cpp
@@ -0,0 +1,10 @@
+// 문제3. 두 수의 곱
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+// num1과 num2를 곱한 정수를 리턴
+int solution(int num1, int num2) {
+    int answer = num1 * num2;
+    return answer;
+}
